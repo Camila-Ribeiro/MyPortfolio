a19c8fdcc55eea256392503fde0f61db1eceeba5
Camila <camilarf@gmail.com>
2020-11-12 17:54:36 +0000

    created static, css and js folders, refactor index to one-page layout

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Camila Ribeiro"
+    return render_template("index.html", title=title)
 
 @app.route("/portifolio")
 def portifolio():
