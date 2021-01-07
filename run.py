8a90a896c155d574fc5c74ef4734227d7b0e584e
Camila <camilarf@gmail.com>
2021-01-07 16:20:52 +0000

    deleted unused route/html contact.html and code refactor on contct section/index.html

--- run.py
+++ run.py
@@ -38,9 +38,9 @@ def project_detail(project_name):
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
