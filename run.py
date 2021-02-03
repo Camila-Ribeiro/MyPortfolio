b3e083bd70a0098d1fafa3ced252a95d80f59c59
Camila <camilarf@gmail.com>
2021-02-03 13:08:53 +0000

    Refactor nav on base.html & run.py, update json

--- run.py
+++ run.py
@@ -14,22 +14,31 @@ def index():
     data = []
     with open("data/projects.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("index.html", title=title, index=index, projects=projects, about=about, contact=contact, portifolio=data)
+    return render_template("index.html", title=title, index=index, projects=projects, about=about, contact=contact, my_projects=data)
 
 @app.route("/projects/<project_name>")
 def project_detail(project_name):
     project = {}
-    
+
+    title = "Camila Ribeiro - Projects"
+    projects = "../../#projects"
+    about = "#about"
+    contact = "../../#contact"
+
     with open("data/projects.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == project_name:
                 project = obj
-    return render_template("project-detail.html", project=project)
+    return render_template("project-detail.html",title=title, projects=projects, about=about, contact=contact, project=project)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", about=about)
+    title = "Camila Ribeiro - About"
+    projects = "../../#projects"
+    # about = "#about"
+    contact = "../../#contact"
+    return render_template("about.html", title=title, index=index, projects=projects, contact=contact, about=about)
 
 
 if __name__ == "__main__":
