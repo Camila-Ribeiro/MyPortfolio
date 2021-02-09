8b8675370fded3a7d66e11d3bcbeb791f21915a0
Camila <camilarf@gmail.com>
2021-02-09 23:19:06 +0000

    Clean up code and refactor main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,20 @@
-/* When the user scrolls down, hide the navbar.
-  When the user scrolls up, show the navbar */
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
+/* When the user scrolls down, hide the navbar.When the user scrolls up, show the navbar */
+$( document ).ready(function() {
+  $('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+});
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-74px";
-  }
+if (prevScrollpos <= 0) {
+  document.getElementById("navbar").style.top = "0";
+}
+else if(prevScrollpos > currentScrollPos) {
+  document.getElementById("navbar").style.top = "0";
+} else {
+  document.getElementById("navbar").style.top = "-74px";
+}
   prevScrollpos = currentScrollPos;
 }
 
